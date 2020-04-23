9ceb2e6cf2a9e167a9870c28d2bf9ef21c14d3dc
_ <53088156+636f7374@users.noreply.github.com>
2020-04-23 21:48:28 +0800

    Context.all_transport -> Transport.

--- src/herbal/context.cr
+++ src/herbal/context.cr
@@ -19,30 +19,6 @@ module Herbal
       @remote
     end
 
-    private def uploaded_size=(value : UInt64)
-      @uploadedSize = value
-    end
-
-    private def uploaded_size
-      @uploadedSize
-    end
-
-    private def received_size=(value : UInt64)
-      @receivedSize = value
-    end
-
-    private def received_size
-      @receivedSize
-    end
-
-    private def maximum_timed_out=(value : Int32)
-      @maximumTimedOut = value
-    end
-
-    private def maximum_timed_out
-      @maximumTimedOut || 64_i32
-    end
-
     def stats
       Stats.from_socket client
     end
@@ -82,91 +58,8 @@ module Herbal
     end
 
     def transport
-      all_transport client, remote
-    end
-
-    # I ’m going to make a long talk here, about why I did this (all_transport)
-    # Some time ago, I found that if the client is writing data to Remote, if Remote is also reading at the same time, it will trigger the problem of read timeout, and vice versa.
-    # So far, I still don't know if it is a bad implementation of Crystal (IO::Evented).
-    # So I thought of this solution.
-    # When the timeout is triggered, immediately check whether the other party (upload / receive) has completed the transmission, otherwise continue to loop IO.copy.
-    # At the same time, in order to avoid the infinite loop problem, I added the maximum number of attempts.
-    # This ensures that there is no disconnection when transferring data for a long time.
-    # Taking a 30-second timeout as an example, 30 * maximum number of attempts (default: 64) = 1920 seconds
-
-    def all_transport(client, remote : IO)
-      spawn do
-        timed_out_counter = 0_u64
-        exception = nil
-        count = 0_u64
-
-        loop do
-          size = begin
-            IO.copy client, remote, true
-          rescue ex : IO::CopyException
-            exception = ex.cause
-            ex.count
-          rescue
-            nil
-          end
-
-          size.try { |_size| count += _size }
-          break if maximum_timed_out <= timed_out_counter
-
-          case exception
-          when IO::TimeoutError
-            timed_out_counter += 1_i32
-            next sleep 0.05_f32.seconds unless received_size
-          else
-          end
-
-          break
-        end
-
-        self.uploaded_size = count || 0_u64
-      end
-
-      spawn do
-        timed_out_counter = 0_u64
-        exception = nil
-        count = 0_u64
-
-        loop do
-          size = begin
-            IO.copy remote, client, true
-          rescue ex : IO::CopyException
-            exception = ex.cause
-            ex.count
-          rescue
-            nil
-          end
-
-          size.try { |_size| count += _size }
-          break if maximum_timed_out <= timed_out_counter
-
-          case exception
-          when IO::TimeoutError
-            timed_out_counter += 1_i32
-            next sleep 0.05_f32.seconds unless uploaded_size
-          else
-          end
-
-          break
-        end
-
-        self.received_size = count || 0_u64
-      end
-
-      spawn do
-        loop do
-          if uploaded_size || received_size
-            client.close rescue nil
-            break remote.close rescue nil
-          end
-
-          sleep 1_i32.seconds
-        end
-      end
+      _transport = Transport.new client, remote
+      _transport.perform
     end
 
     def perform
